d5d02cae66359958978a550f280a65858f5ad788
Israel <israelaguiar619@gmail.com>
2025-06-25 16:26:29 -0300

    Revisando listas no array

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const limpar = document.querySelector(".apagarCampo");
 
 const icon = document.querySelector(".icons"); //PAI CLASS
 
+const itens = document.querySelector(".itens-lista")
+
 const editar = document.querySelector(".editar"); //TAREFA
 const apagar = document.querySelector(".apagar"); //TAREFA
 
-const limparTudo = document.querySelector(".btn-limpar")
+const limparTudo = document.querySelector(".btn-limpar");
 
 let msg = document.querySelector(".msg-input");
 
@@ -20,67 +22,72 @@ editar.addEventListener("click", editarTarefa);
 apagar.addEventListener("click", deletarTarefa);
 limparTudo.addEventListener("click", limparTarefas);
 
-function adicionarTarefa() {  
-     
-  if(campo.value == "") {
+function adicionarTarefa() {
+  
+
+  if (campo.value.trim() === "") {
     msg.textContent = "Escreva uma tarefa";
     msg.style.color = "red";
   } 
-  
   else {
-    tarefas.push(campo.value);
-    let listasTarefas = document.querySelector(".itens-lista"); //UL DA LISTA
-    listasTarefas.innerHTML = ""
+    if (tarefas.includes(campo.value)) {
+      msg.textContent = "Tarefa já existe";
+      msg.style.color = "red";
+    } 
+    
+    else {
+      let listasTarefas = document.querySelector(".itens-lista");
 
-    msg.textContent = "Tarefa adicionada";
-    msg.style.color = "#1CCE3A";
+      listasTarefas.innerHTML = "";
 
-    console.log(tarefas)
-    //console.log(tarefas)
-    for (let i = 0; i < tarefas.length; i++) {
+      for(let i = 0; i < tarefas.length; i++){
+      // tarefas.push(campo.value);
+
       let novaLI = document.createElement("li");
       novaLI.classList.add("lista-item");
-      // <li class="lista-item">
-      listasTarefas.appendChild(novaLI);
 
       let p_item = document.createElement("p");
       p_item.classList.add("texto-item");
-      novaLI.appendChild(p_item);
-      //<p class="texto-item">Estudar HTML</p>
       p_item.textContent = tarefas[i];
-      p_item.value = "";
-      console.log(p_item.value);
+
       let novaDiv = document.createElement("div");
       novaDiv.classList.add("icons");
-      //  <div class="icons"> </div>
-      novaLI.appendChild(novaDiv);
 
       let novoI1 = document.createElement("i");
       novoI1.classList.add("fa-solid", "fa-pen", "editar", "icon");
-      // <i class="fa-solid fa-pen editar icon"></i>
-      novaDiv.appendChild(novoI1);
+
       let novoI2 = document.createElement("i");
       novoI2.classList.add("fa-solid", "fa-trash", "apagar", "icon");
+
+      novaDiv.appendChild(novoI1);
       novaDiv.appendChild(novoI2);
 
-      
-      // console.log(tarefas);
-      // console.log(`${tarefas}`, tarefas.indexOf[i])
+      novaLI.appendChild(p_item);
+      novaLI.appendChild(novaDiv);
+      listasTarefas.appendChild(novaLI);
+
+      msg.textContent = "Tarefa adicionada";
+      msg.style.color = "#1CCE3A";
+
+      // campo.value = ""; // limpa o input
+
+      console.log(tarefas)
+      }
     }
   }
 }
 
+
+
 function deletarTarefa() {
   //let index = tarefas.indexOf(dado)
- // console.log(index)
+  // console.log(index)
 }
 
-function editarTarefa() {
+function editarTarefa() {}
 
-}
-
-function limparTarefas(){
-  tarefas.splice(0, tarefas.length)
+function limparTarefas() {
+  tarefas.splice(0, tarefas.length);
 }
 
 function limparCampo() {
